038478613b9327af03f3f4fafba9d06372b76929
creepahh <creepahh13@gmail.com>
2025-08-14 13:40:46 +0545

    Add Cypress tests with Mochawesome reporting and package scripts

--- cypress/e2e/transfer.cy.js
+++ cypress/e2e/transfer.cy.js
@@ -1,9 +1,29 @@
+describe('Transfer Funds - Parabank', () => {
+  const baseUrl = 'http://parabank.parasoft.com';
 
-cy.visit('http://parabank.parasoft.com');
-cy.get('input[name="username"]').type('john');
-cy.get('input[name="password"]').type('demo');
-cy.get('input[type="submit"]').click();
-cy.contains('Transfer Funds').click();
-cy.get('input[name="amount"]').type('100');
-cy.get('input[type="submit"]').click();
-cy.contains('Transfer Complete').should('exist');
+  beforeEach(() => {
+    cy.visit(`${baseUrl}/parabank/index.htm`);
+
+    // Login
+    cy.get('input[name="username"]').type('john');
+    cy.get('input[name="password"]').type('demo');
+    cy.get('input[type="submit"]').click();
+
+    // Ensure login success
+    cy.contains('Accounts Overview').should('be.visible');
+  });
+
+  it('transfers funds successfully between accounts', () => {
+    // Navigate to the Transfer Funds page before filling the form
+    cy.contains('Transfer Funds').click();
+
+    // Now fill the form inputs
+    cy.get('input[name="amount"]').type('100');
+
+    // Submit the transfer
+    cy.get('input[type="submit"]').click();
+
+    // Assert success message
+    cy.contains('Transfer Complete!').should('be.visible');
+  });
+});
