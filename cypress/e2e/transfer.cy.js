f966fe5978531b579943ebbfde203898eda560e5
creepahh <creepahh13@gmail.com>
2025-08-16 17:19:07 +0545

    chore: add custom login command and update transfer funds test to use it

--- cypress/e2e/transfer.cy.js
+++ cypress/e2e/transfer.cy.js
@@ -2,28 +2,15 @@ describe('Transfer Funds - Parabank', () => {
   const baseUrl = 'http://parabank.parasoft.com';
 
   beforeEach(() => {
-    cy.visit(`${baseUrl}/parabank/index.htm`);
-
-    // Login
-    cy.get('input[name="username"]').type('john');
-    cy.get('input[name="password"]').type('demo');
-    cy.get('input[type="submit"]').click();
-
-    // Ensure login success
-    cy.contains('Accounts Overview').should('be.visible');
+    cy.loginParabank('kri', 'demo');  
   });
 
   it('transfers funds successfully between accounts', () => {
-    // Navigate to the Transfer Funds page before filling the form
     cy.contains('Transfer Funds').click();
 
-    // Now fill the form inputs
     cy.get('input[name="amount"]').type('100');
-
-    // Submit the transfer
     cy.get('input[type="submit"]').click();
 
-    // Assert success message
-    cy.contains('Transfer Complete!').should('be.visible');
+    cy.contains('Transfer Complete!').should('be.visible'); // Assert success
   });
 });
